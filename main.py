4b8bdf4e61b05a78dd1e61a83199b8060038a48e
Diego Mancevo <dmancevo@gmail.com>
2021-04-06 20:09:50 +0200

    added independent axes and adjusting x-axis.

--- main.py
+++ main.py
@@ -21,68 +21,86 @@ ELEMENT_COL = "elem"
 
 THEME_IMAGE = plt.imread("https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/1280px-Olympic_rings_without_rims.svg.png", format="png")
 
+IMAGES = [
+    plt.imread("https://www.libertygames.co.uk/images/desc/Sonic-Logo(3).jpg", format='jpeg')
+for _ in range(7)]
+
 N = 400
 df = pd.concat([pd.DataFrame(dict(
     y=np.random.uniform(0,10,N).cumsum(),
     date=[date(2020,1,1)+timedelta(days=i) for i in range(N)],
-    elem=[e for _ in range(N)])) for e in ['a','b','c','d','e']])
+    elem=[e for _ in range(N)])) for e in ['sonic','diego','sam','frodo','gendry', 'player_1', 'player_2']])
 
 df.sort_values([DATE_COL, METRIC], inplace=True)
 min_date = df[DATE_COL].min()
 n_elements = df[ELEMENT_COL].nunique()
 
-IMAGES = [
-    plt.imread("https://www.libertygames.co.uk/images/desc/Sonic-Logo(3).jpg", format='jpeg')
-for _ in range(n_elements)]
-
+# Figure and axes.
 fig = plt.figure(figsize=(20,12))
-ax = plt.axes(
-    xlim=(0, df[METRIC].max()),
+ax = fig.add_subplot( # Use this axes for the bars.
+    xlim=(0, df[METRIC].head(n_elements * 100).max()),
     ylim=(0, n_elements + 2.3),
-    position=[0,.2,.9,.9],
+    position=[.15,.2,.9,.9],
+    frame_on=False,
 )
-ax.axis('off')
+ax0 = fig.add_subplot( # Use this axes for names/logos.
+    position=[0,.2,.15,.9],
+    frame_on=False,
+    ylim=(0, n_elements + 2.3)
+)
+ax1 = fig.add_subplot(position=[0,0,1,.2], frame_on=False) # Use this axes for date and theme picture.
+for _ax in [ax, ax0, ax1]:
+    _ax.axis('off')
+# Artists.
 bars = ax.barh(
     y=[0 for i in range(n_elements)],
     width=[1 for _ in range(n_elements)],
     color=sns.color_palette()[:n_elements],
-    alpha=.7,
+    alpha=.6,
 )
-curr_date = ax.text(x=0.5, y=0.07, s="", alpha=0.7, fontdict={
+curr_date = ax1.text(x=0.1, y=0.5, s="", alpha=0.7, fontdict={
     "fontfamily": "sans-serif",
-    "fontsize":90,
+    "fontsize":120,
 })
 labels = [ax.text(
-    x=0, y=0, s="0", fontdict={
+    x=0, y=0, s="", fontdict={
         "fontfamily": "sans-serif",
         "fontsize":"medium",
         "horizontalalignment": "center",
     },
 ) for _ in range(n_elements)]
-imagebox = OffsetImage(THEME_IMAGE, zoom=0.37)
-ab = AnnotationBbox(imagebox, (.5, .5), xybox=(df[METRIC].max()*0.9, 0.01), frameon=False)
+imagebox = OffsetImage(THEME_IMAGE, zoom=0.32)
+ab = AnnotationBbox(imagebox, xy=(.5, .5), xybox=(0.8, 0.7), frameon=False)
 ab.set_animated(True)
-ax.add_artist(ab)
+ax1.add_artist(ab)
 images = []
 for pic in IMAGES:
     imagebox = OffsetImage(pic, zoom=0.5)
-    ab = AnnotationBbox(imagebox, (.5, .5), frameon=False)
+    ab = AnnotationBbox(imagebox, xy=(.5, .5), frameon=False)
     ab.set_animated(True)
-    ax.add_artist(ab)
+    ax0.add_artist(ab)
     images.append(ab)
+
+# Animation.
 def update(i):
     d = min_date + timedelta(days=i)
     _df = df[df[DATE_COL]==d]\
         .assign(Rank = lambda x: x[METRIC].rank())
+    # Adjust the x-axis to keep up with the growing numbers.
+    a,b = ax.get_xlim()
+    m = _df[METRIC].max()
+    if 0.8 * b < m:
+        ax.set_xlim(a, m/0.8)
+    # Update artists.
     curr_date.set_text(f"{d.year} {d.strftime('%b')}")
-    for t, patch, txt, ab in zip(_df.iterrows(), bars.patches, labels, images):
+    for t, patch, txt, elem in zip(_df.iterrows(), bars.patches, labels, images):
         _, row = t
         patch.set_width(row[METRIC])
         patch.set_y(row.Rank)
         txt.set_text(f"{row[METRIC]:.2f}")
-        txt.set_x(row[METRIC])
-        txt.set_y(row.Rank)
-        ab.xybox=(row[METRIC]+2, row.Rank+.5)
+        txt.set_x(row[METRIC]*1.05)
+        txt.set_y(row.Rank + patch.get_height()/2.5)
+        elem.xybox=(0.5, row.Rank + patch.get_height()/2.5)
     return bars.patches + labels + images + [curr_date]
 anim = FuncAnimation(fig, update, frames=N, interval=40, blit=True)
 anim.save('plot.mp4', writer='ffmpeg')
