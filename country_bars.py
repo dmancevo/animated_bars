a19d107ae3ff4f35ebac718e4bbc335fe84f9f1f
Diego Mancevo <dmancevo@gmail.com>
2021-04-17 14:55:07 +0200

    CO2 Emissions per Country.

--- country_bars.py
+++ country_bars.py
@@ -16,10 +16,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 sns.set_style("white")
 sns.set_context("poster")
+#sns.set_palette("YlGn")
 sns.set_palette("rocket")
-
-
-df = pd.read_csv("API_NY/API_NY.GDP.PCAP.KD_DS2_en_csv_v2_2163576.csv", skiprows=4)
 
 
 def get_bars(df:pd.DataFrame, start_year=1960, end_year=2019) -> list[tuple[list[str], list[float]]]:
@@ -32,22 +30,23 @@ def get_bars(df:pd.DataFrame, start_year=1960, end_year=2019) -> list[tuple[list
                 break
         df[f"{year+1}"].fillna(df[f"{year}"], inplace=True)
         for alpha in np.arange(0,1.05,.05):
-            df['GDP Percapita'] = (1-alpha) * df[f"{year}"] + alpha * df[f"{year+1}"]
-            df.sort_values('GDP Percapita', ascending=False, inplace=True)
+            df['CO2 emissions (kt)'] = (1-alpha) * df[f"{year}"] + alpha * df[f"{year+1}"]
+            df.sort_values('CO2 emissions (kt)', ascending=False, inplace=True)
             bars.append((year if alpha < 1 else year+1,
                          df['Country Name'].head(20).tolist(),
-                         df['GDP Percapita'].head(20).tolist()))
+                         df['CO2 emissions (kt)'].head(20).tolist()))
     return bars
 
 
 def add_image(
         ax:matplotlib.axes.Axes,
-        ticker:str, zoom:float, xybox:tuple[float,float]
+        name:str, zoom:float, xybox:tuple[float,float]
     ) -> matplotlib.offsetbox.AnnotationBbox:
     try:
-        image = plt.imread(f"ticker_images/{ticker}.png")
-    except FileNotFoundError:
-        return None
+        image = plt.imread(f"flags/{name}.png")
+    except Exception as ex:
+        print(name)
+        raise(ex)
     imagebox = OffsetImage(image, zoom=zoom)
     ab = AnnotationBbox(imagebox, xy=(.5,.5), xybox=xybox, frameon=False)
     ab.set_animated(True)
@@ -70,7 +69,7 @@ def get_update(
                 colors[c] = palette[np.random.choice(len(palette))]
             col.append(colors[c])
         barC = plt.barh(np.arange(len(countries), 0, -1), values, tick_label=countries, color=col)
-        plt.title(f"Year: {year}", fontsize=50)
+        plt.title(f"CO2 Emissions (kt) Year: {year}", fontsize=50)
         ax = plt.gca()
         for spine in ax.spines.values():
             spine.set_visible(False)
@@ -78,12 +77,18 @@ def get_update(
             l.set_fontsize(20)
         plt.tick_params(left=False, bottom=False, labelbottom=False)
         width=max([bar.get_width() for bar in barC])
-        for bar in barC:
+        for country, bar in zip(countries, barC):
             ax.text(
                 bar.get_width(),
                 bar.get_y() + .2 * bar.get_height(),
-                f"  ${bar.get_width():.0f}", va='center',
+                f"  {bar.get_width():.0f}", va='center',
                 fontsize=20,
+            )
+            add_image(
+                plt.gca(),
+                country,
+                .25,
+                (bar.get_width() + .09*width, bar.get_y() + .5*bar.get_height())
             )
     return update
 
@@ -96,5 +101,45 @@ def make_anim(bars:list[tuple[list[str],list[float]]], file_name:str="mov.mp4", 
         interval=100, frames=len(bars), repeat=False,
     )
     anim.save(file_name, writer='ffmpeg')
-    
-make_anim(get_bars(df, 2010, 2020))
+
+if __name__ == "__main__":
+    df = pd.read_csv("API_EN.ATM.CO2E.KT_DS2_en_csv_v2_2163797.csv", skiprows=4)
+    for x in [
+        "World",
+        "IDA & IBRD total",
+        "Low & middle income",
+        "Middle income",
+        "IBRD only",
+        "Upper middle income",
+        "High income",
+        "Late-demographic dividend",
+        "OECD members",
+        "East Asia & Pacific",
+        "Post-demographic dividend",
+        "Europe & Central Asia",
+        "Early-demographic dividend",
+        "North America",
+        "Lower middle income",
+        "European Union",
+        "Euro area",
+        "Middle East & North Africa",
+        "South Asia",
+        "Latin America",
+        "Sub-Saharan Africa",
+        "IDA total",
+        "Central Europe and the Baltics",
+        "Fragile and conflict affected situations",
+        "IDA blend",
+        "Pre-demographic dividend",
+        "IDA only",
+        "Least developed countries",
+        "Low income",
+    ]:
+        df = df[~df['Country Name'].str.contains(x)]
+    df['Country Name'] = df['Country Name']\
+        .apply(lambda s: "Iran" if s=="Iran, Islamic Rep." \
+                                 else "South Korea" if s=="Korea, Rep." \
+                                 else "North Korea" if s=="Korea, Dem. People’s Rep." \
+                                 else s)
+    make_anim(get_bars(df, 1960, 2020))
+    run("bash add_music_and_export.sh", shell=True, capture_output=True, check=True)
--- flags/download.sh
+++ flags/download.sh
@@ -1,7 +1,7 @@
-for url in `wget -O- https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags | grep -o //upload.wikimedia.org/wikipedia/commons/thumb/[a-z0-9][a-z0-9]*/[a-z0-9][a-z0-9]*/Flag_of_[_%0-9a-zA-z]*.svg/[0-9][0-9]*px-Flag_of_[_%0-9a-zA-z]*.svg.png | sort | uniq`
+for url in `wget -O- https://en.wikipedia.org/wiki/Gallery_of_sovereign_state_flags | grep -o //upload.wikimedia.org/wikipedia/commons/thumb/[a-z0-9]*/[a-z0-9]*/Flag_of_[_%0-9a-zA-Z]*.svg/[0-9][0-9]*px-Flag_of_[_%0-9a-zA-Z]*.svg.png | sort | uniq`
 do
-    file_name=`echo $url | grep -o Flag_of_[A-Z][a-z]*.svg.png | sed s/Flag_of_//g | sed s/\.svg//g`
-    echo $file_name $url
+    file_name=`echo $url | grep -o Flag_of_[_%0-9a-zA-Z]*.svg.png | sed s/Flag_of_//g | sed s/\.svg//g`
+    echo $file_name
     if [ -f $file_name ]; then
         continue
     else
